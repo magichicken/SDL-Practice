231dc2e8900a934b6093f2a10fe62dd78de62df7
Default User <Default@defaults-macbook-pro.local>
2015-12-24 22:39:25 -0600

    Initial Commit

---
+++ SDL-Practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SDL-Practice
+//
+//  Created by Default User on 12/24/15.
+//  Copyright © 2015 Default. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
